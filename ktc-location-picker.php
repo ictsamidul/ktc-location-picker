160b773ba09a8d608c5edc30c6e386594b77b1dd
Samidul Islam <56181908+wpwebguru@users.noreply.github.com>
2020-04-24 02:00:50 +0600

    Update ktc-location-picker.php

--- ktc-location-picker.php
+++ ktc-location-picker.php
@@ -113,7 +113,7 @@
         // Create the search box and link it to the UI element.
         var input = document.getElementById('billing_address_1', 'billing_city');
         var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+       // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
         // Bias the SearchBox results towards current map's viewport.
         map.addListener('bounds_changed', function() {
